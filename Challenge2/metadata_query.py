427a0940fee7b5be02e11a5dee713c635ffc84d0
Prudhvi1236 <40086994+Prudhvi1236@users.noreply.github.com>
2023-07-10 09:32:13 +0530

    Create metadata_query.py

---
+++ Challenge2/metadata_query.py
@@ -0,0 +1,20 @@
+# using google help i got this Python script. 
+import json
+import subprocess
+
+def get_azure_instance_metadata(key=None):
+    command = "az vm show -d --query instanceView -o json"
+    output = subprocess.check_output(command, shell=True).decode('utf-8')
+    data = json.loads(output)
+
+    if key:
+        return data.get(key)
+    else:
+        return data
+
+# Example usage
+metadata = get_azure_instance_metadata()
+
+# Pretty-print the metadata as JSON
+formatted_metadata = json.dumps(metadata, indent=4)
+print(formatted_metadata)
